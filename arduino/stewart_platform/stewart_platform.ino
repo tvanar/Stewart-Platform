fb38a4c9fd9a71ccdb9681d74b16f1cba7dd993a
Tvånar <eandr98@gmail.com>
2025-04-02 15:27:43 +0200

    first commit

---
+++ arduino/stewart_platform/stewart_platform.ino
@@ -0,0 +1,52 @@
+#include <ArduinoEigen.h>
+
+
+class Kinematics {
+  private:
+    float base_radius;
+    float platform_radius;
+    float phi_base; // RADIANS
+    float phi_platform; // RADIANS
+
+    InverseKinematics()
+    
+
+
+
+
+
+  public:
+    // Constructor
+    Kinematics(float base_radius, float platform_radius, float phi_base, float phi_platform) {
+      this->base_radius = base_radius;
+      this->platform_radius = platform_radius;
+      this->phi_base = phi_base;
+      this->phi_platform = phi_platform;
+    }
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void setup(){
+
+}
+
+
+void loop(){
+
+}
