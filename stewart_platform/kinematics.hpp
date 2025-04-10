6721a9d71b598ec7906d9d90ca5c5e6af85bfba3
Tvånar <eandr98@gmail.com>
2025-04-10 10:02:13 +0200

    Finished Kinematics

--- stewart_platform/kinematics.hpp
+++ stewart_platform/kinematics.hpp
@@ -1,6 +1,7 @@
 #ifndef KINEMATICS
 #define KINEMATICS
 
+#include <math.h>
 #include <BasicLinearAlgebra.h>
 using namespace BLA;
 
@@ -51,8 +52,8 @@ class Kinematics {
     void setUpPlatform() {
       for(int i = 0; i < NUM_LEGS; i++) {
         int j = (i % 2 == 1) ? -1 : 1;
-        base_attachments[i] = {base_radius * cos(angles_rad[i] + j * phi_base / 2.0f), base_radius * sin(angles_rad[i] + j * phi_base / 2.0), 0.0};
-        platform_attachments[i] = {platform_radius * cos(angles_rad[i] + j * phi_platform / 2.0f), platform_radius * sin(angles_rad[i] + j * phi_platform / 2.0), 0.0};
+        base_attachments[i] = {base_radius * cos(angles_rad[i] + j * phi_base / 2.0), base_radius * sin(angles_rad[i] + j * phi_base / 2.0), 0.0};
+        platform_attachments[i] = {platform_radius * cos(angles_rad[i] + j * phi_platform / 2.0), platform_radius * sin(angles_rad[i] + j * phi_platform / 2.0), 0.0};
       }
     }
 
@@ -60,26 +61,32 @@ class Kinematics {
       return sqrt(leg_vec(0)*leg_vec(0) + leg_vec(1)*leg_vec(1) + leg_vec(2)*leg_vec(2)); 
     }
 
-    // double norm(double leg_vec[6]) {
-    //   double cubesum = 0;
-    //   for(int i = 0; i < NUM_LEGS; i++) {
-    //     cubesum += leg_vec[i]*leg_vec[i];
-    //   }
-    //   return sqrt(cubesum);
-    // }
+    double norm(double leg_vec[6]) {
+      double cubesum = 0;
+      for(int i = 0; i < NUM_LEGS; i++) {
+        cubesum += leg_vec[i]*leg_vec[i];
+      }
+      return sqrt(cubesum);
+    }
+
+    double norm(BLA::Matrix<6,1,double> leg_vec) {
+      double cubesum = 0;
+      for(int i = 0; i < NUM_LEGS; i++) {
+        cubesum += leg_vec(i)*leg_vec(i);
+      }
+      return sqrt(cubesum);
+    }
 
     BLA::Matrix<3,1,double> cross(BLA::Matrix<3,1,double> a, BLA::Matrix<3,1,double> b) {
       return {a(1)*b(2) - a(2)*b(1), a(2)*b(0) - a(0)*b(2), a(0)*b(1) - a(1)*b(0)};
     }
 
-
-    //, BLA::Matrix<3,3,double> rot
-    void rotationAsEuler(double angles[DIM]) {
+    void rotationAsEuler(double angles[DIM], BLA::Matrix<3,3,double>& rot) {
       double cz = cos(angles[0]), sz = sin(angles[0]);  // Z-axis
       double cy = cos(angles[1]), sy = sin(angles[1]);  // Y-axis
       double cx = cos(angles[2]), sx = sin(angles[2]);  // X-axis
 
-      r = {
+      rot = {
         // Column 0
         cy*cz,
         cz*sx*sy - cx*sz,
@@ -95,12 +102,29 @@ class Kinematics {
       };
     }
     
+
+    /*
+    * Takes a 3x3 rotation matrix as input and edits the last three elements of est_pose
+    */
+    void rotationFromEuler(BLA::Matrix<3,3,double> m, BLA::Matrix<6,1>& est_pose) {
+      double psi; // alpha
+      double theta; //beta
+      double phi; // gamma
+      if(m(2,0) != 1 && m(2,0) != -1) { // maybe remove.. never pm1 in our usecase.
+        theta = - asin(m(2,0));
+        double cth = cos(theta);
+        psi = atan2(m(2,1)/cth,m(2,2)/cth);
+        phi = atan2(m(1,0)/cth,m(0,0)/cth);
+      }
+
+    }
+    
     /*
     * Returns the lengths of the legs based on a pose using a vector-loop equation.
     */
     void inverseKinematics(double pose[NUM_LEGS]){
       double euler_angles[3] = {pose[5], pose[4], pose[3]};
-      rotationAsEuler(euler_angles);
+      rotationAsEuler(euler_angles,r);
       BLA::Matrix<3,1,double> pose_as_matrix = {pose[0], pose[1], pose[2]};
       for(int i = 0; i < NUM_LEGS; i++){
         legs[i] = pose_as_matrix + (r * platform_attachments[i]) - base_attachments[i];
@@ -109,11 +133,10 @@ class Kinematics {
       }
     }
 
-
-    // maybe preallocate
     void calculateJacobian() {
+      Matrix<3,1,double> cross_vector;
       for(int i = 0; i < NUM_LEGS; i++) {
-        Matrix<3,1,double> cross_vector = cross(r * platform_attachments[i], unit_legs[i]);
+        cross_vector = cross(r * platform_attachments[i], unit_legs[i]);
         jacobian(i,0) = unit_legs[i](0);
         jacobian(i,1) = unit_legs[i](1);
         jacobian(i,2) = unit_legs[i](2);
@@ -125,6 +148,7 @@ class Kinematics {
 
   public:
     double position[6] = {0,0,0,0,0,0};
+    int iter = 0;
     /*
     * CONSTRUCTOR
     */
@@ -136,44 +160,104 @@ class Kinematics {
     /*
     * Returns the position of the platform based on the lengths of the legs using Newton-Raphson.
     */
-    // bool forwardKinematics(float initial_guess[6],float measured_lengths[6]) {
-    //   // ROTATION based on guess
-    //   double euler_angles[3] = {pose[5], pose[4], pose[3]};
-    //   rotationAsEuler(euler_angles,r);
-      
-    //   /*
-    //   * Building homogenus transformation matrix
-    //   */ 
-    //   for(int i = 0;i < DIM; i++) {
-    //     for(int j = 0; j < DIM; j++) {
-    //       Kinematics::est_pose(i,j) = r(i,j);
-    //     }
-    //     Kinematics::est_pose(i,3) = pose[i];
-    //   }
-    //   Kinematics::est_pose(3,0) = 0;
-    //   Kinematics::est_pose(3,1) = 0;
-    //   Kinematics::est_pose(3,2) = 0;
-    //   Kinematics::est_pose(3,3) = 1;
+   bool forwardKinematics(double initial_guess[6], double measured_lengths[6]) {
+    // ROTATION based on guess
+    double euler_angles[3] = {initial_guess[5], initial_guess[4], initial_guess[3]};
+    rotationAsEuler(euler_angles, r);
+    
+    /*
+    * Building homogenus transformation matrix
+    */ 
+    for(int i = 0; i < DIM; i++) {
+        for(int j = 0; j < DIM; j++) {
+            Kinematics::est_pose(i,j) = r(i,j);
+        }
+        Kinematics::est_pose(i,3) = initial_guess[i];
+    }
+    Kinematics::est_pose(3,0) = 0;
+    Kinematics::est_pose(3,1) = 0;
+    Kinematics::est_pose(3,2) = 0;
+    Kinematics::est_pose(3,3) = 1;
 
-    //   // Iteration variables
-    //   double delta_lengths[6] = {0.0,0.0,0.0,0.0,0.0,0.0};
-    //   int count = 0;
-    //   double error = 1 + TOLERANCE;
-    //   double legs_est[6];
+    // Iteration variables
+    Matrix<6,1,double> delta_lengths = {0.0,0.0,0.0,0.0,0.0,0.0};
+    BLA::Matrix<6,1,double> estimated_pose_vector;
+    BLA::Matrix<6,1,double> delta_x;
+    int count = 0;
+    double error = 1 + TOLERANCE;
+    double legs_est[6];
+    BLA::Matrix<6,6,double> lambda = BLA::Eye<6,6,double>();
+    lambda = lambda * 0.001;
+    BLA::Matrix<3,1,double> x_delta_rot;
+    BLA::Matrix<4,4,double> delta_T;
+    BLA::Matrix<4,4,double> estimated_pose = est_pose;
 
-    //   while(error > TOLERANCE && count < 100) { //maybe change to 50?
-    //     // define new variables
-    //     Kinematics::inverseKinematics();
-    //     legs_est = Kinematics::legs_length;
-    //     for(int i = 0; i < NUM_LEGS; i++) {
-    //       delta_lengths[i] = measured_lengths[i]-legs_est[i];
-    //     }
-    //     error = Kinematics::norm(delta_lengths);
-    //     Kinematics::calculateJacobian();
+    while(error > TOLERANCE && count < MAX_INTERATIONS) {
+        // define new variables
+        double current_pose[6] = {
+            estimated_pose(0,3),
+            estimated_pose(1,3),
+            estimated_pose(2,3),
+            euler_angles[2], // roll
+            euler_angles[1], // pitch
+            euler_angles[0]  // yaw
+        };
+        inverseKinematics(current_pose);
+        
+        for(int i = 0; i < NUM_LEGS; i++) {
+            legs_est[i] = Kinematics::legs_length[i];
+            delta_lengths(i) = measured_lengths[i]-legs_est[i];
+        }
+        error = Kinematics::norm(delta_lengths);
+        
+        //calculate jacobian matrix
+        Kinematics::calculateJacobian();
+        Kinematics::jacobian_transpose = ~Kinematics::jacobian;
+        
+        /* FIND DELTA_X from jacobian using pseudoinverse */
+        // Calculating psuedoinverse
+        Kinematics::j_star = BLA::Inverse(Kinematics::jacobian_transpose*Kinematics::jacobian + lambda)*Kinematics::jacobian_transpose;
+        
+        // Calculating delta_x
+        delta_x = Kinematics::j_star*delta_lengths;
 
-    //   }
+        // Calculating x_delta_rot & new rot matrix
+        x_delta_rot = {delta_x(5)*ALPHA_ROT, delta_x(4)*ALPHA_ROT, delta_x(3)*ALPHA_ROT};
+        for(int i = 0; i < DIM; i++) {
+            euler_angles[i] += x_delta_rot(i);
+        }
+        rotationAsEuler(euler_angles, r_delta);
+        delta_T = {
+            r_delta(0,0), r_delta(0,1), r_delta(0,2), delta_x(0),
+            r_delta(1,0), r_delta(1,1), r_delta(1,2), delta_x(1),
+            r_delta(2,0), r_delta(2,1), r_delta(2,2), delta_x(2),
+            0, 0, 0, 1
+        };
+        
+        estimated_pose = delta_T * estimated_pose;
+        
+        estimated_pose_vector = {
+            estimated_pose(0,3),
+            estimated_pose(1,3),
+            estimated_pose(2,3),
+            euler_angles[2], // roll
+            euler_angles[1], // pitch
+            euler_angles[0]  // yaw
+        };
 
-    // }
+        error = Kinematics::norm(delta_lengths);
+        count++;
+        iter++;
+    }
+
+    if(count < MAX_INTERATIONS) {
+        for(int i = 0; i < 6; i++) {
+            Kinematics::position[i] = estimated_pose_vector(i);
+        }
+        return true;
+    }
+    return false;
+  }
 
 
 
--- stewart_platform/stewart_platform.ino
+++ stewart_platform/stewart_platform.ino
@@ -1,4 +1,4 @@
-#include "kinematics_double.hpp"
+#include "kinematics.hpp"
 #include <BasicLinearAlgebra.h>
 using namespace BLA;
 
@@ -11,31 +11,27 @@ void setup() {
     const double PHI_PLATFORM = 1.396263;
     double POSE[6] = {0.0,0.0,1.0,0.08726,0.0,0.0};
     while(!Serial);
-      // Instantiate the Kinematics object locally within setup()
     Kinematics kinematics(BASE_RADIUS, PLATFORM_RADIUS, PHI_BASE, PHI_PLATFORM);
     BLA::Matrix<6,1,double> m = kinematics.runInverseKinematics(POSE);
 
-    
+    BLA::Matrix<6,6,double> m2 = kinematics.getJacobian();
+
     Serial.println(m);
 
-    double POSE2[6] = {0.0,0.1,1.0,0.0,0.0,0.0};
+    printM(m2);
 
-    BLA::Matrix<6,1,double> m2 = kinematics.runInverseKinematics(POSE2);
-
-    Serial.println(m2);
-
-    // BLA::Matrix<3,3,double> t =kinematics.getR();
-
-    // Serial.println(t(1,1),10);
+    double legs[6] = {31.17,31.93,31.03,30.88,31.50,30.88};
+    double guess[6] = {0.0,0.0,1.0,0.0826,0.0,0.0};
     
-    // Matrix<6,6,double> j = kinematics.getJacobian();
-    // printM(j);
+    kinematics.forwardKinematics(guess,legs);
+    for(int i = 0;i < 6;i++) {
+      Serial.print(kinematics.position[i],5);
+      Serial.print(" ");
+    }
 
-    // BLA::Matrix<3,6,double> m2 = kinematics.getUnitLegs();
-    // Serial.println("");
-    // Serial.println(m2);
-
-    // Serial.println(kinematics.getLegs());
+    Serial.println(kinematics.iter);
+    // BLA::Matrix<3,3,double> t = kinematics.getR();
+    // print3M(t);
 
     
     }
@@ -46,6 +42,16 @@ void setup() {
     for(int j = 0; j < 6; j++) {
         Serial.print(m(i,j), 10); // Print with specified precision
         if(j < 6 - 1) Serial.print(" "); // Add space between elements
+      }
+      Serial.println(); // New line after each row
+    }
+  }
+
+  void print3M(BLA::Matrix<3,3,double> m) {
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 3; j++) {
+        Serial.print(m(i,j), 10); // Print with specified precision
+        if(j < 3 - 1) Serial.print(" "); // Add space between elements
       }
       Serial.println(); // New line after each row
     }
