7ba1096088c4852d41d0bea24534d8db754bc475
Eddie-pd <71640119+Eddie-pd@users.noreply.github.com>
2025-04-14 09:15:19 +0200

    Add files via upload

---
+++ Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.cpp
@@ -0,0 +1,5 @@
+#include "Output2Keystrokes.hpp"
+
+int doubleIt(int value) {
+    return value * 2;
+}
---
+++ Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.ino
@@ -0,0 +1,11 @@
+#include "Output2Keystrokes.hpp" //Här inkluderar vi c++ filen med alla våra funktioner och kod i.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
