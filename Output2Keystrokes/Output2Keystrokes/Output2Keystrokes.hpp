26e790f4b26fe8f3daaa9f295707f0f2a5914b0f
Eddie-pd <eddiepd.w@gmail.com>
2025-04-29 09:19:07 +0200

    comments

--- Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.hpp
+++ Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.hpp
@@ -1,6 +1,9 @@
 #ifndef Output2Keystrokes_HPP
 #define Output2Keystrokes_HPP
 
-int doubleIt(int value);
+//Här skriver vi kod för att ta alla positionsvärden och vinkelvärden från Enars vektor och....
+//....beroende på dessa värden så ska vi ge olika tangentslag. Typ om musen är väldigt åt höger..
+//.. så ska kanske tangent d tryckas på, och sen är nästa steg pwm som hade gjort att beroende...
+//..på hur mycket åt höger vi går, desto mer frekvent ska tangenten tryckas.
 
 #endif
