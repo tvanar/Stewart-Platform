799cca091db8562b2de9d6e915d0ab2f3d34dd11
Eddie-pd <eddiepd.w@gmail.com>
2025-04-29 12:00:23 +0200

    changes

--- Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.hpp
+++ Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.hpp
@@ -1,6 +1,11 @@
 #ifndef Output2Keystrokes_HPP
 #define Output2Keystrokes_HPP
 
+#include <Arduino.h>
+#include <math.h>
+#include <BasicLinearAlgebra.h>
+#include "kinematics.hpp"
+
 //Här skriver vi kod för att ta alla positionsvärden och vinkelvärden från Enars vektor och....
 //....beroende på dessa värden så ska vi ge olika tangentslag. Typ om musen är väldigt åt höger..
 //.. så ska kanske tangent d tryckas på, och sen är nästa steg pwm som hade gjort att beroende...
