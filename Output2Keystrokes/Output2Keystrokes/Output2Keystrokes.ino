ee6edbfb3d89374a68d195417ca3517f66a7bc60
Eddie-pd <eddiepd.w@gmail.com>
2025-04-29 11:30:18 +0200

    Update Output2Keystrokes.ino

--- Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.ino
+++ Output2Keystrokes/Output2Keystrokes/Output2Keystrokes.ino
@@ -1,5 +1,5 @@
 #include "Output2Keystrokes.hpp" //Här inkluderar vi c++ filen med alla våra funktioner och kod i.
-#include <keyboard.h>
+#include <Keyboard.h>
 //Jag gör såhär att jag skriver all koden utan funktioner här först, gör sen om det till funktioner som kan kodas i hpp filen och bara använder de funktionerna här sen.
 
 //Set up our variables of the position and the angles.
@@ -20,36 +20,40 @@ void setup() {
 void loop() { // Here i will make a simple example to try out the concept, wether we go beyond the negative limit or positive limit in any direction doesn't matter in this simple example, they both give the same result. Can make more complex later if we want to.
   if(fabs(x_val) < x_0 && fabs(y_val) < y_0 && fabs(z_val) < z_0){
     Serial.println("Musen är i nolläge");
-  }
-  if(fabs(x_val) >  x_0){ //if we are far enough in the positive or negative direction, press key.
+  } 
+  else{
+    if(fabs(x_val) >  x_0){ //if we are far enough in the positive or negative direction, press key.
     keyboard.press(a);//press a tangent
     delay(100);
     keyboard.release();
   }
-  if(fabs(y_val) > y_0){ //if we are far enough in the positive or negative direction, press key.
-    keyboard.press(b);//press a different tangent
-    delay(100);
-    keyboard.release();
+    if(fabs(y_val) > y_0){ //if we are far enough in the positive or negative direction, press key.
+      keyboard.press(b);//press a different tangent
+      delay(100);
+      keyboard.release();
   }
-  if(fabs(z_val) > z_0){ //if we are far enough in the positive or negative direction, press key.
-    keyboard.press(c);//press a different tangent
-    delay(100);
-    keyboard.release();
+    if(fabs(z_val) > z_0){ //if we are far enough in the positive or negative direction, press key.
+      keyboard.press(c);//press a different tangent
+      delay(100);
+      keyboard.release();
   }
-  if(fabs(roll_val) > roll_0){ //if we are far enough in the positive or negative direction, press key.
-    keyboard.press(c);//press a different tangent
-    delay(100);
-    keyboard.release();
+    if(fabs(roll_val) > roll_0){ //if we are far enough in the positive or negative direction, press key.
+      keyboard.press(c);//press a different tangent
+      delay(100);
+      keyboard.release();
   }
-  if(fabs(yaw_val) > yaw_0){ //if we are far enough in the positive or negative direction, press key.
-    keyboard.press(d);//press a different tangent
-    delay(100);
-    keyboard.release();
+    if(fabs(yaw_val) > yaw_0){ //if we are far enough in the positive or negative direction, press key.
+      keyboard.press(d);//press a different tangent
+      delay(100);
+      keyboard.release();
   } 
-  if(fabs(pitch_val) > pitch_0){ //if we are far enough in the positive or negative direction, press key.
-    keyboard.press(e);//press a different tangent
-    delay(100);
-    keyboard.release();
+    if(fabs(pitch_val) > pitch_0){ //if we are far enough in the positive or negative direction, press key.
+      keyboard.press(e);//press a different tangent
+      delay(100);
+      keyboard.release();
   }
+
+  }
+  
 
 }
