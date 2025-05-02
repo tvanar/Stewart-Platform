1918a7236c27b9f11ffbb81bf61fd65cd8b33bde
Jackamotto <paydayjgmm@gmail.com>
2025-05-02 11:08:00 +0200

    flyttade metoder till hpp fil

--- Output2Keystrokes/Output2Keystrokes/3Dmus.ino
+++ Output2Keystrokes/Output2Keystrokes/3Dmus.ino
@@ -35,24 +35,4 @@ void loopFunction() {
   checkAndPress(pitch, pitch_0, 'i', 'k'); // Pitch+ = upp, Pitch- = ner
 }
 
-int computeDelay(double value, double threshold) {
-  double delta = fabs(value) - threshold;
-  if (delta <= 0) return 0;
-  double delayTime = MAX_DELAY - (delta / threshold) * (MAX_DELAY - MIN_DELAY);
-  return constrain(delayTime, MIN_DELAY, MAX_DELAY);
-}
-
-void checkAndPress(double value, double threshold, char posKey, char negKey) {
-  if (fabs(value) > threshold) {
-    int d = computeDelay(value, threshold);
-    if (value > 0) {
-      Keyboard.press(posKey);
-    } else {
-      Keyboard.press(negKey);
-    }
-    delay(d);
-    Keyboard.releaseAll(); 
-  }
-}
-
 #endif
