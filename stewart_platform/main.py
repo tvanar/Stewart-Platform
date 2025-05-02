9232d18cfba33ce5463efa214e2dd1d63f48610d
Vincent <vincent.olson@hotmail.com>
2025-05-02 11:00:48 +0200

    test

--- stewart_platform/main.py
+++ stewart_platform/main.py
@@ -3,3 +3,10 @@ import serial
 import struct
 from scipy.spatial.transform import Rotation as R
 
+
+
+def legLength(V_x):
+    V_2 = 4.91
+    V_1 = 0.226
+    F = 30/(V_2-V_1)*(V_x-V_1) + 149
+    return F
